5970f775d229b53cd3db4df91ada8617c2d6766f
ultimatemethod <diandr8@naver.com>
2023-12-04 14:57:35 +0900

    TouchManager.cs 수정

--- Assets/_/_jongho/Scripts/TouchManager.cs
+++ Assets/_/_jongho/Scripts/TouchManager.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class TouchManager : MonoBehaviour
 {
     private ARRaycastManager arRaycastManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
     
-    public GameObject petObject;
+    private Vector3 indicatorPos;
+    private Feed pet;
+    public GameObject petStatus;
 
     private void Awake()
     {
@@ -27,46 +27,40 @@ public class TouchManager : MonoBehaviour
             // 레이캐스트를 생성하고 UI 레이어에서 레이캐스트 히트를 확인
             Ray ray = Camera.main.ScreenPointToRay(touch.position);
             RaycastHit hit;
-            
+
             // UI 터치
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, 5))
-            {                
+            {
+                petStatus.SetActive(false);
                 return;
             }
             // 펫 터치
-            else if (Physics.Raycast(ray, out hit, Mathf.Infinity, 7)) //  펫 layer로 수정
+            else if (Physics.Raycast(ray, out hit, Mathf.Infinity, 6))
             {
-                // 펫을 해당 위치로 이동시키는 함수 호출
+                // 펫 상태창 팝업
+                petStatus.SetActive(true);
             }
             else
             {
+                indicatorPos = PlaceOnIndicator.currentAim.position;
 
                 // 선택된 상태 체크해서 
                 // 선택 상태 아니라면
-                if (PlaceOnIndicator.placePrefab != null)
+                if (PlaceOnIndicator.placePrefab == null)
                 {
-
+                    // 펫을 해당 위치로 이동시키는 함수 호출
+                    pet = PetStatManager.Instance.GetCurrentPet().GetComponent<Feed>();
+                    // pet.MoveToThere(indicatorPos);
                 }
                 // 선택 상태라면
                 else
                 {
-
-                }                    
-
-                    // 터치된 곳이 plane detection을 통해 인식된 바닥이라면, 특정 오브젝트를 해당 위치로 이동
-                    if (arRaycastManager.Raycast(touch.position, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
-                {
-                    
-                    
-
+                    // 인디케이터 위치에 해당 오브젝트 instantiate
+                    Instantiate(PlaceOnIndicator.placePrefab, indicatorPos, Quaternion.identity);                    
+                    PlaceOnIndicator.placePrefab = null;
                 }
-                //
-                else
-                {
-
-                }
-            }            
+            }
         }
     }
-    
+
 }
